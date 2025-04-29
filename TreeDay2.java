00914f3c8b3128f3c2edfa374bad6853c11e1cc2
pranav-2258 <156687951+pranav-2258@users.noreply.github.com>
2025-04-29 11:17:25 +0530

    TreeDay2.java

--- TreeDay2.java
+++ TreeDay2.java
@@ -1,42 +1,43 @@
 public class TreeDay2 {
     
-    static class TreeNode{
+    static class TreeNode {
         int data;
         TreeNode left, right;
-        TreeNode(int value){
+
+        TreeNode(int value) {
             data = value;
             left = right = null;
-            
         }
     }
 
-    public static int sumOfNodes(TreeNode root) {
+    // Renamed method from sumOfNodes to calculateTreeSum
+    public static int calculateTreeSum(TreeNode root) {
         if (root == null) return 0;
-        return root.data + sumOfNodes(root.left) + sumOfNodes(root.right);
+        return root.data + calculateTreeSum(root.left) + calculateTreeSum(root.right);
     }
 
-    public static void printKLevel(TreeNode root, int k) {
+    // Renamed method from printKLevel to displayLevelNodes
+    public static void displayLevelNodes(TreeNode root, int level) {
         if (root == null) return;
-        if (k == 1) {
+        if (level == 1) {
             System.out.print(root.data + " ");
         } else {
-            printKLevel(root.left, k - 1);
-            printKLevel(root.right, k - 1);
+            displayLevelNodes(root.left, level - 1);
+            displayLevelNodes(root.right, level - 1);
         }
     }
 
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         TreeNode root = new TreeNode(10);
         root.left = new TreeNode(20);
         root.right = new TreeNode(30);
         root.left.left = new TreeNode(40);
         root.right.right = new TreeNode(50);
- 
-        System.out.println("Sum of all nodes: " + sumOfNodes(root));
+
+        System.out.println("Sum of all nodes: " + calculateTreeSum(root));
 
         System.out.print("Nodes at level 2: ");
-        printKLevel(root, 2);
+        displayLevelNodes(root, 2);
         System.out.println();
     }
 }
